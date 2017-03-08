faecb83d7baf4d8fbe3ce5e9c5f3e12550f2449f
shreyansh <shreyansh.barodiya@gmail.com>
2017-03-09 02:52:43 +0530

    added stars field in reviews

--- treeplantationdb.sql
+++ treeplantationdb.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 20, 2016 at 06:40 PM
--- Server version: 5.7.16-0ubuntu0.16.10.1
--- PHP Version: 7.0.8-3ubuntu3
+-- Generation Time: Mar 09, 2017 at 02:51 AM
+-- Server version: 5.7.17-0ubuntu0.16.10.1
+-- PHP Version: 7.0.15-0ubuntu0.16.10.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -57,8 +57,7 @@ CREATE TABLE `directory` (
 --
 
 INSERT INTO `directory` (`tree_id`, `directory_id`, `uploaded_by`, `clicked_on`) VALUES
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'shreyanshb', '2016-12-16'),
-('sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997', '1', 'shreyanshb', '2016-12-05');
+('shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894', '1', 'shreyanshb', '2017-02-16');
 
 -- --------------------------------------------------------
 
@@ -77,14 +76,8 @@ CREATE TABLE `image` (
 --
 
 INSERT INTO `image` (`tree_id`, `directory_id`, `image_url`) VALUES
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'http://192.168.1.2/treePlantation/uploads/shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae-1-0.jpg'),
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'http://192.168.1.2/treePlantation/uploads/shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae-1-1.jpg'),
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'http://192.168.1.2/treePlantation/uploads/shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae-1-2.jpg'),
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'http://192.168.1.2/treePlantation/uploads/shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae-1-3.jpg'),
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'http://192.168.1.2/treePlantation/uploads/shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae-1-4.jpg'),
-('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', '1', 'http://192.168.1.2/treePlantation/uploads/shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae-1-5.jpg'),
-('sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997', '1', 'http://192.168.1.2/treePlantation/uploads/sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997-1-0.jpg'),
-('sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997', '1', 'http://192.168.1.2/treePlantation/uploads/sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997-1-1.jpg');
+('shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894', '1', 'http://192.168.0.106/treePlantation/uploads/shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894-1-0.jpg'),
+('shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894', '1', 'http://192.168.0.106/treePlantation/uploads/shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894-1-1.jpg');
 
 -- --------------------------------------------------------
 
@@ -98,23 +91,17 @@ CREATE TABLE `review` (
   `review_text` text,
   `reviewed_by` varchar(50) DEFAULT NULL,
   `reviewed_on` date DEFAULT NULL,
-  `title` varchar(100) DEFAULT NULL
+  `title` varchar(100) DEFAULT NULL,
+  `review_stars` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `review`
 --
 
-INSERT INTO `review` (`tree_id`, `review_no`, `review_text`, `reviewed_by`, `reviewed_on`, `title`) VALUES
-('shreyanshb_aca68f04-fa79-40b0-9772-c5513e96adb8', '1', 'DG en DX us', 'shreyanshb', '2016-12-12', 'DJ'),
-('shreyanshb_aca68f04-fa79-40b0-9772-c5513e96adb8', '2', 'DJ en is\n', 'shreyanshb', '2016-12-20', 'if LR HD'),
-('shreyanshb_ebea8423-579d-4155-93fb-686d928fcff9', '1', 'DJ Kathie\n', 'shreyanshb', '2016-12-28', 'TV UC if'),
-('shreyanshb_ebea8423-579d-4155-93fb-686d928fcff9', '2', 'GH just HDTV positive prideful el Beth idk startup phytase etch Joey in Ethiopia Columbia  \nvery good tree I like it very much I loved it very very very much very good app', 'sunnyb1234', '2017-10-20', 'sunny kw review'),
-('shreyanshb_ef5279fd-dc7d-4a76-8cfa-a0d0ecfc5887', '1', 'she\naiiak', 'shreyanshb', '2016-12-22', 'sksjcjs'),
-('shreyanshb_ef5279fd-dc7d-4a76-8cfa-a0d0ecfc5887', '2', 'Dahlia shipwreck debit servo still Webelo ideal keen Karachi IEC Khan Kathi auburn unanimous flabbergasted\n\n\nChristy Jan ignore Hubli uninterested\n\n\nHugo DS week Ivory own\n\nBeth Beth', 'sunnyb1234', '2016-01-02', 'sunny ka review'),
-('shreyanshb_ef5279fd-dc7d-4a76-8cfa-a0d0ecfc5887', '3', 'FYI LP\n\nhonk\njunk', 'sunnyb1234', '2016-12-30', 'DJ is'),
-('sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997', '1', 'happy first year birthday tree', 'sunnyb1234', '1997-09-22', 'first review '),
-('sunnyb1234_50816810-c4e2-49e9-b4a1-1012509cc997', '2', 'cracks CK ka shines go Oshawa heavy', 'shreyanshb', '2010-09-02', 'checking done');
+INSERT INTO `review` (`tree_id`, `review_no`, `review_text`, `reviewed_by`, `reviewed_on`, `title`, `review_stars`) VALUES
+('shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894', '1', 'very great condition', 'admin', '2017-03-10', 'great', 5),
+('shreyanshb_822bd0b3-4e3e-43d7-a011-f26f5f5f4c55', '1', 'kV on of if if kg kg kg', 'shreyanshb', '2017-03-09', 'ajfdkkalkkc', 2.5);
 
 -- --------------------------------------------------------
 
@@ -136,7 +123,12 @@ CREATE TABLE `tree` (
 --
 
 INSERT INTO `tree` (`tree_id`, `username`, `latitude`, `longitude`, `planted_on`, `species`) VALUES
+('admin_671e710e-b17a-44fe-80e0-6e7994c488e0', 'admin', '22.75108473', '77.73653135', '2017-03-07', 'hbd'),
+('admin_a2b45cfe-21f1-4bfb-95e5-6d4550393b63', 'admin', '19.13436759', '72.91012120', '2017-03-09', 'campus hub'),
+('admin_fec55fb5-0ae4-4939-bff6-860a7071359e', 'admin', '19.09244794', '72.85701681', '2017-03-08', 'airport'),
 ('shreyanshb_1308515b-1cc3-43dd-bebc-419b83f2a1d2', 'shreyanshb', '18.92267035', '72.83382107', '2016-11-26', 'taj mahal'),
+('shreyanshb_75b7e076-2318-439e-b16a-ed0cc0d80894', 'shreyanshb', '19.13702988', '72.90996931', '2017-02-14', 'h4room25'),
+('shreyanshb_822bd0b3-4e3e-43d7-a011-f26f5f5f4c55', 'shreyanshb', '19.13607013', '72.91051615', '2017-01-26', 'randomMumbai'),
 ('shreyanshb_a864c1c7-b9dd-4cd3-8297-750e9db33549', 'shreyanshb', '19.12059048', '72.88547371', '2016-12-20', 'Ghent SSH GS'),
 ('shreyanshb_aca68f04-fa79-40b0-9772-c5513e96adb8', 'shreyanshb', '22.74791303', '77.73878071', '2016-09-02', 'Sibiu'),
 ('shreyanshb_dae1c4cb-ae14-4999-8a0b-6413b50be2ae', 'shreyanshb', '22.74661131', '77.74221931', '2016-07-06', 'helpless'),
@@ -163,8 +155,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `name`, `password`, `email`, `ph_no`) VALUES
-('dtdft', 'tx', 'ea7020f28ef8d80b443373847614dde7f4f6193a', 'gx@xy.ii', '7987413194'),
-('dtdftg', 'gig', 'ee759b1936af4d7bd0d0e5363e6b1ea438e6a390', 'chug@CH.j', '6889'),
+('admin', 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@admin.admin', '9967063383'),
 ('shreyanshb', 'Shreyansh Barodiya', 'a159cdb85f6fa619d1512bacb60a87d8e6291a0c', 'shreyansh.barodiya@gmail.com', '9967063383'),
 ('sunnyb1234', 'Sunny', 'd2884b15701413489adf952c64ca644f80da4b16', '140050025@iitb.ac.in', '7987413194');
 
