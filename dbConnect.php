8ece7be49c6ab43ad5f7745fc06e5dc38c99830b
shreyansh <shreyansh.barodiya@gmail.com>
2017-05-14 10:57:03 +0530

    Changes in the Config and DbConnect files

--- dbConnect.php
+++ dbConnect.php
@@ -1,7 +1,7 @@
 <?php
  define('HOST','localhost');
- define('USER','root');
- define('PASS','insanity');
+ define('USER','treeplant');
+ define('PASS','tree012xt');
  define('DB','treeplantationdb');
  
  $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
